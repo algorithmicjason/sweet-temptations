f2e08d8dc6f081f85bdb5a064a0db1ae4d0c6186
Jason Herrera <baytownjason@yahoo.com>
2022-03-10 06:05:04 -0600

    routes controller views communicating for Devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'user#index'
+  get '/ideas', to: 'ideas#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '*path', to: 'user#index', via: :all
 end
